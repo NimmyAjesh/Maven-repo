52d752c876a89e66c8585999f514263fd91f211a
nimmyajesh <nimmyajesh@LAPTOP-EIBVFOB8>
2024-03-25 11:59:29 +0530

    hi to coding

---
+++ My_Project/src/main/java/maven_project/SampleProgram.java
@@ -0,0 +1,11 @@
+package maven_project;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to java");
+	}
+
+}
+
+ 
---
+++ My_Project/src/main/java/maven_project/Sample_Project2.java
@@ -0,0 +1,9 @@
+package maven_project;
+
+public class Sample_Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to selenium");
+	}
+
+}
